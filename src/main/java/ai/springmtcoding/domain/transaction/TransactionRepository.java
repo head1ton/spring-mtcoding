a1f4d5c3a922dc453165009e7b48ebcc1c1d6fd5
head1ton <head1ton@gmail.com>
2023-09-20 02:37:38 +0900

    repository 생성

---
+++ src/main/java/ai/springmtcoding/domain/transaction/TransactionRepository.java
@@ -0,0 +1,7 @@
+package ai.springmtcoding.domain.transaction;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+}
---
+++ src/main/java/ai/springmtcoding/domain/user/UserRepository.java
@@ -0,0 +1,7 @@
+package ai.springmtcoding.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
