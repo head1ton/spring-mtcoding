b9d651cfd69bfd28dd05bdda9b3fbd830459ba12
head1ton <head1ton@gmail.com>
2023-09-20 00:44:29 +0900

    빈 확인

--- src/main/java/ai/springmtcoding/SpringMtcodingApplication.java
+++ src/main/java/ai/springmtcoding/SpringMtcodingApplication.java
@@ -2,6 +2,7 @@ package ai.springmtcoding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -10,6 +11,11 @@ public class SpringMtcodingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringMtcodingApplication.class, args);
+//        ConfigurableApplicationContext context = SpringApplication.run(SpringMtcodingApplication.class, args);
+//        String[] iocNames = context.getBeanDefinitionNames();
+//        for (String iocName : iocNames) {
+//            System.out.println(iocName);
+//        }
     }
 
 }
