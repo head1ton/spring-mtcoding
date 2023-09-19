b85c2a9f211b8b1d025dcbe72f4ce5c8f67ccc6f
head1ton <head1ton@gmail.com>
2023-09-19 23:08:55 +0900

    기본설정 셋팅

---
+++ src/main/java/ai/springmtcoding/SpringMtcodingApplication.java
@@ -0,0 +1,13 @@
+package ai.springmtcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMtcodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMtcodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ai/springmtcoding/SpringMtcodingApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.springmtcoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMtcodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
