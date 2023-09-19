44f0ce72d501c81cf8f0362941ed5dd7554d0770
head1ton <head1ton@gmail.com>
2023-09-19 23:43:59 +0900

    유저 테이블 생성, 날짜 자동 등록

---
+++ src/main/java/ai/springmtcoding/domain/user/User.java
@@ -0,0 +1,77 @@
+package ai.springmtcoding.domain.user;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "user_tb")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 20)
+    private String username;
+
+    @Column(nullable = false, length = 60)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String email;
+
+    @Column(nullable = false, length = 20)
+    private String fullname;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserEnum role;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public User(
+        final Long id,
+        final String username,
+        final String password,
+        final String email,
+        final String fullname,
+        final UserEnum role,
+        final LocalDateTime createdAt,
+        final LocalDateTime updatedAt) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.fullname = fullname;
+        this.role = role;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
