bb27001ffd9088f3f68fb6687dbbc3d156ced7d0
head1ton <head1ton@gmail.com>
2023-09-21 22:32:53 +0900

    AccountRepository Query Sample

--- src/main/java/ai/springmtcoding/domain/account/AccountRepository.java
+++ src/main/java/ai/springmtcoding/domain/account/AccountRepository.java
@@ -4,12 +4,15 @@ import java.util.List;
 import java.util.Optional;
 import javax.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     // select * from account where number = :number
     // TODO : 고객 관련 정보 가져와야하니.
-    Optional<Account> findByNumber(Long number);
+//    @Query("SELECT ac FROM Account ac JOIN FETCH ac.user u WHERE ac.number = :number")
+    Optional<Account> findByNumber(@Param("number") Long number);
 
     List<Account> findByUserId(Long id);
 }
