de297bf40e9bb80b8600e2baee56156eb36dfed5
head1ton <head1ton@gmail.com>
2023-09-21 16:19:00 +0900

    본인계좌 목록보기 Service

--- src/main/java/ai/springmtcoding/domain/account/AccountRepository.java
+++ src/main/java/ai/springmtcoding/domain/account/AccountRepository.java
@@ -1,5 +1,6 @@
 package ai.springmtcoding.domain.account;
 
+import java.util.List;
 import java.util.Optional;
 import javax.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,6 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     // select * from account where number = :number
     // TODO : 고객 관련 정보 가져와야하니.
     Optional<Account> findByNumber(Long number);
+
+    List<Account> findByUser_id(Long id);
 }
--- src/main/java/ai/springmtcoding/service/AccountService.java
+++ src/main/java/ai/springmtcoding/service/AccountService.java
@@ -7,8 +7,13 @@ import ai.springmtcoding.domain.user.UserRepository;
 import ai.springmtcoding.dto.account.AccountReqDto.AccountSaveReqDto;
 import ai.springmtcoding.handler.ex.CustomApiException;
 import ai.springmtcoding.dto.account.AccountRespDto.AccountSaveRespDto;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +24,45 @@ public class AccountService {
 
     private final UserRepository userRepository;
     private final AccountRepository accountRepository;
+
+    // 고객별 계좌목록 보기
+    public AccountListRespDto accountList_user(Long userId) {
+        User userPS = userRepository.findById(userId).orElseThrow(
+            () -> new CustomApiException("고객을 찾을 수 없습니다.")
+        );
+
+        List<Account> accountListPS = accountRepository.findByUser_id(userId);
+        return new AccountListRespDto(userPS, accountListPS);
+    }
+
+    @Getter
+    @Setter
+    public static class AccountListRespDto {
+
+        private String fullname;
+        private List<AccountDto> accounts = new ArrayList<>();
+
+        public AccountListRespDto(User user, final List<Account> accounts) {
+            this.fullname = user.getFullname();
+            this.accounts = accounts.stream().map(AccountDto::new).collect(
+                Collectors.toList());
+        }
+
+        @Getter
+        @Setter
+        public static class AccountDto {
+
+            private Long id;
+            private Long number;
+            private Long balance;
+
+            public AccountDto(Account account) {
+                this.id = account.getId();
+                this.number = account.getNumber();
+                this.balance = account.getBalance();
+            }
+        }
+    }
 
     @Transactional
     public AccountSaveRespDto accountCreate(AccountSaveReqDto accountSaveReqDto, Long userId) {
@@ -39,5 +83,4 @@ public class AccountService {
         // DTO 로 응답
         return new AccountSaveRespDto(accountPS);
     }
-
 }
